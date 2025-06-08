f6330b342dcfe046ec19d1d195f5e27f7e5b335f
manojnatekar <manojnatekar95@gmail.com>
2025-06-08 17:04:50 +0530

    reverting commiting ThirdClass java file after modification

--- src/main/java/ThirdClass.java
+++ src/main/java/ThirdClass.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class ThirdClass {
     public static void main(String[] args) {
-        System.out.println("Third class after modified");
+        System.out.println("Third class");
     }
 }
